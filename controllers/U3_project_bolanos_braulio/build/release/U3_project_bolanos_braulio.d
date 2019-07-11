14d17b863510d47a8bd9cb401ba77357eec4f5c9
Braulio <al1609017@upy.edu.mx>
2019-07-11 13:29:42 -0500

    Begin

---
+++ controllers/U3_project_bolanos_braulio/build/release/U3_project_bolanos_braulio.d
@@ -0,0 +1,7 @@
+build/release/U3_project_bolanos_braulio.o: U3_project_bolanos_braulio.c \
+ /usr/local/webots/include/controller/c/webots/robot.h \
+ /usr/local/webots/include/controller/c/webots/types.h \
+ /usr/local/webots/include/controller/c/webots/nodes.h \
+ /usr/local/webots/include/controller/c/webots/motor.h \
+ /usr/local/webots/include/controller/c/webots/distance_sensor.h \
+ /usr/local/webots/include/controller/c/webots/position_sensor.h
